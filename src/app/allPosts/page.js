0d165d60b8c8856622d013b3372e1db191a819d4
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-07 13:49:52 -0500

    handle get comments and delete posts

--- src/app/allPosts/page.js
+++ src/app/allPosts/page.js
@@ -5,9 +5,11 @@ import { useEffect, useState } from "react"
 export default function AllPosts() {
 
     const [data, setData] = useState(null)
+    const [comments, setComment] = useState(null)
 
     useEffect(() => {
-        getData()
+        getData(),
+        getComments()
     },[])
 
     const getData = async() => {
@@ -31,6 +33,50 @@ export default function AllPosts() {
 
     }
 
+    const getComments = async() => {
+        try {
+            const res = await fetch("http://localhost:3000/blog-api/comments/allComments")
+
+            const dataResponse = await res.json()
+
+            if(res.ok) {
+                setComment(dataResponse)
+            } else {
+                console.error(dataResponse.message)
+                setComment([])
+            }
+            
+        } catch (error) {
+            console.error("Error to get comments, ", error)
+            setComment([])
+        }
+    }
+
+    const handlePostDelete = async (postId) => {
+        const accessToken = localStorage.getItem('accessToken')
+        try {
+            const res = await fetch(`http://localhost:3000/blog-api/delete/post/${postId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': accessToken,
+                }
+            })
+            
+            if(res.ok) {
+                getData()
+            } else {
+                const errorData = await res.json()
+                throw new Error(errorData.message || "Failed to delete post");
+            }
+
+
+        } catch (error) {
+            console.error(error)
+            
+        }
+    }
+
     return (
         <div>
             <h1>All Posts</h1>
@@ -39,8 +85,25 @@ export default function AllPosts() {
                     <ul>
                         {data.map((post) => (
                             <li key={post._id}>
+                            <p>{post.timeStamp}</p>
                             <h2>{post.title}</h2>
                             <p>{post.content}</p>
+                            <button onClick={() => handlePostDelete(post._id)}>delete</button>
+
+                            {comments ? (
+                            <div>
+                                
+
+                                
+                                {comments.filter((comment) => comment.post == post._id).map((comment) => (
+                                    <p key={comment._id}>{comment.comment}</p>
+                                ))}
+                            
+                            </div>
+                            ): 
+                            <p>No comments yet</p>
+                            }
+
                             </li>
                         ))}
                     </ul>
